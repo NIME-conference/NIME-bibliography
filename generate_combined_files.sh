0f60bccd2ecde697a118e51bf03722e050532a0b
Alexander Refsum Jensenius <arjensenius@gmail.com>
2020-09-07 12:43:25 +0200

    Add script to make combined bib files

---
+++ generate_combined_files.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+cat paper_proceedings/nime20* > paper_proceedings/nime_papers.bib
+cat music_proceedings/nime20* > music_proceedings/nime_music.bib
+cat installation_proceedings/nime20* > installation_proceedings/nime_installations.bib
